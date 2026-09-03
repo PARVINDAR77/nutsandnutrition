c732e2171dfc645a3d0ad731e840c5dc23b363ad
PARVINDAR77 <panjabiparvindar77@gmail.com>
2026-09-03 15:36:35 +0530

    chore: install project dependencies and initialize vendor and node_modules packages

--- laravel-react-app/resources/js/main.jsx
+++ laravel-react-app/resources/js/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/global.css'
-import App from './App.jsx'
+import App from './app.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { AutoRefreshProvider } from './context/AutoRefreshContext'
 
